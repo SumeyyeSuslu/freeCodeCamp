d0580d1df032ec4150be217c7cf4da9e4678da97
SumeyyeSuslu <suslu.sumeyye@gmail.com>
2017-07-22 23:14:08 -0500

    Create returnvaluefromfunction.js

---
+++ returnvaluefromfunction.js
@@ -0,0 +1,10 @@
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+function timesFive(number){
+  return number * 5;
+}
+var answer = timesFive(10);

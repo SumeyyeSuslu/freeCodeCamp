aa5081da2dfc2fb52c6fd384aea8236009853e2b
SumeyyeSuslu <suslu.sumeyye@gmail.com>
2017-07-23 22:18:15 -0500

    Create lookups.js

---
+++ lookups.js
@@ -0,0 +1,40 @@
+// Setup
+function phoneticLookup(val) {
+  var result = "";
+
+  // Only change code below this line
+  /*switch(val) {
+    case "alpha": 
+      result = "Adams";
+      break;
+    case "bravo": 
+      result = "Boston";
+      break;
+    case "charlie": 
+      result = "Chicago";
+      break;
+    case "delta": 
+      result = "Denver";
+      break;
+    case "echo": 
+      result = "Easy";
+      break;
+    case "foxtrot": 
+      result = "Frank";
+  }*/
+   result = {
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot": "Frank",
+    
+  };
+
+  // Only change code above this line
+  return result[val];
+}
+
+// Change this value to test
+phoneticLookup("charlie");

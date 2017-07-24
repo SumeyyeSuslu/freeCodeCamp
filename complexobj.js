f4c3d34289f2be77b1dc8784f5e039ff7b2d0bbf
SumeyyeSuslu <suslu.sumeyye@gmail.com>
2017-07-23 23:00:29 -0500

    Create complexobj.js

---
+++ complexobj.js
@@ -0,0 +1,25 @@
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [ 
+      "CS", 
+      "8T", 
+      "LP" ],
+    "gold": true
+  },
+  // Add record here
+  
+  {
+    "artist": "Tarkan",
+    "title": "Seni Seviyorum",
+    "release_year": 2017,
+    "formats": [
+      "cs",
+      "8T",
+      "lp"],
+    "gold": false
+  }
+];

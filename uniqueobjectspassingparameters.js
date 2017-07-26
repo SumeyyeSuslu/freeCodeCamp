14d217e3e10d9baf5d83fa3ee94015d975d444e6
SumeyyeSuslu <suslu.sumeyye@gmail.com>
2017-07-26 16:25:15 -0500

    Create uniqueobjectspassingparameters.js

---
+++ uniqueobjectspassingparameters.js
@@ -0,0 +1,12 @@
+var Car = function(wheels, seats, engines) {
+  //Change this constructor
+  //this.wheels = 4;
+  //this.seats = 5;
+  //this.engines = 1;
+  this.wheels = wheels;
+  this.seats = seats;
+  this.engines = engines;
+};
+
+//Try it out here
+var myCar = new Car(6, 3, 1);
